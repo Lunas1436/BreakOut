9b48e8f6f13bef2e52816b9e9a049a4a10615b4b
Lunas1436 <naru1436t@gmail.com>
2026-05-05 23:47:51 +0900

    Gameクラス作成

---
+++ GLBreakOut/GLBreakOut/Game.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#ifndef GAME_H
+#define GAME_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+enum GameState {
+	GAME_ACTIVE,
+	GAME_MENU,
+	GAME_WIN
+};
+
+class Game
+{
+public:
+	Game(unsigned int width, unsigned int height);
+	~Game();
+
+	void Init();
+	void ProcessInput(float dt);
+	void Update(float dt);
+	void Render();
+	
+public:
+	GameState State;
+	bool Keys[1024];
+	unsigned int Width, Height;
+
+
+};
+
+
+
+
+
+#endif // !GAME_H
+
